a8cc7cda4aff7b6482a485730882ee2e3766c0f0
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-10-31 17:53:02 +0900

    Update QuizFileDao.java

--- src/main/java/com/example/quizapp/QuizFileDao.java
+++ src/main/java/com/example/quizapp/QuizFileDao.java
@@ -1,5 +1,26 @@
 package com.example.quizapp;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizFileDao {
+
     
+    private static final String FILE_PATH = "quizzes.txt";
+
+    public void write(List<Quiz> quizzes) throws IOException {
+        List<String> lines = new ArrayList<>();
+        for (Quiz quiz: quizzes){
+            lines.add(quiz.toString());
+        }
+        Path path = Paths.get(FILE_PATH);
+
+        //第一引数・・・書き込み先のファイルパス
+        //第二引数・・・書き込むデータ
+        Files.write(path, lines);
+    }
 }
